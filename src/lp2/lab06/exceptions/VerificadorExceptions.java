58fdbe35b4f52fad66f2f4b4d8c309c39278f825
Pabllo Lima <pabllo.lima@ccc.ufcg.edu.br>
2016-08-16 21:25:19 -0300

    Classe de Exceptions criada

---
+++ src/lp2/lab06/exceptions/VerificadorExceptions.java
@@ -0,0 +1,33 @@
+package lp2.lab06.exceptions;
+
+public class VerificadorExceptions {
+
+	public static void verificaStringNulaVazia(String string, String tipo) throws Exception {
+		if (string == null) {
+			throw new Exception(tipo + " nao pode ser nulo.");
+		} else if (string.trim().isEmpty()) {
+			throw new Exception(tipo + " nao pode ser vazio.");
+		}
+	}
+
+	public static void verificaDoubleNegativo(double valor, String tipo) throws Exception {
+		if (valor < 0) {
+			throw new Exception(tipo + " nao pode ser negativo.");
+		}
+	}
+
+	public static void verificaDoubleInvalido(double valor, String tipo) throws Exception {
+		if (valor == 0) {
+			throw new Exception(tipo + " nao pode ser igual a zero.");
+		} else if (valor < 0) {
+			throw new Exception(tipo + " nao pode ser negativo.");
+		}
+	}
+	
+	public static void verificaSaldoInsuficiente(double saldo, double precoJogo) throws Exception {
+		if (saldo < precoJogo) {
+			throw new Exception("Saldo insuficiente para comprar o jogo.");
+		}
+	}
+
+}
