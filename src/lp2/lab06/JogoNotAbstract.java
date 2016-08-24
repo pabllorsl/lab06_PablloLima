586c0d99c9bf7dde1bd8eb82eaece96f926dbfd2
Pabllo Lima <pabllo.lima@ccc.ufcg.edu.br>
2016-08-23 22:53:35 -0300

    Projeto renomeado, autoria adicionada

--- src/lp2/lab06/JogoNotAbstract.java
+++ src/lp2/lab06/JogoNotAbstract.java
@@ -1,3 +1,5 @@
+/* 115111825 - Pabllo Rodolfo Santos de Lima: LAB 6 - Turma 1 */
+
 package lp2.lab06;
 
 public class JogoNotAbstract extends Jogo {
--- testes/lp2/lab06/JogoNotAbstractTest.java
+++ testes/lp2/lab06/JogoNotAbstractTest.java
@@ -1,3 +1,5 @@
+/* 115111825 - Pabllo Rodolfo Santos de Lima: LAB 6 - Turma 1 */
+
 package lp2.lab06;
 
 import static org.junit.Assert.*;
